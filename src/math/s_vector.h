506a9895f388dbbe19779849353317706552b6ad
陆伍澪 <80970301+AAA650@users.noreply.github.com>
2025-04-12 18:53:48 +0800

    Delete src directory

--- src/math/s_vector.h
+++
@@ -1,9 +0,0 @@
-// s_vector.h
-// generate vector files
-#include "s_vector_base.h"
-#include "s_vector_2.h"
-#include "s_vector_2i.h"
-#include "s_vector_3.h"
-#include "s_vector_3i.h"
-#include "s_vector_4.h"
-#include "s_vector_4i.h"
--- src/math/s_vector_base.cpp
+++
@@ -1,9 +0,0 @@
-// s_vec_base.cpp
-// implement of SVector
-
-#include "s_vector_base.h"
-
-namespace stapler_engine::math
-{
-
-}
